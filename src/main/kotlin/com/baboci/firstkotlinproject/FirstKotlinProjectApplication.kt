f0f2e03fb26f9ba703de424bc2a94ee5001e4f71
tahirbaboci <tahir.baboci@autoscout24.com>
2021-12-26 11:58:10 +0100

    first commit

---
+++ src/main/kotlin/com/baboci/firstkotlinproject/FirstKotlinProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.baboci.firstkotlinproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FirstKotlinProjectApplication
+
+fun main(args: Array<String>) {
+	runApplication<FirstKotlinProjectApplication>(*args)
+}
---
+++ src/test/kotlin/com/baboci/firstkotlinproject/FirstKotlinProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.baboci.firstkotlinproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FirstKotlinProjectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
